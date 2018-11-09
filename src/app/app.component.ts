3c347107313bb2c22b228d82c1f9cc9065248cbf
icancode23 <ernipunarora@gmail.com>
2018-11-09 20:36:18 +0530

    Try button location changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {SearchItem} from "./searchresult"
-import { GetItemsService } from './get-items.service';
 import { forEach } from '@angular/router/src/utils/collection';
 @Component({
   selector: 'app-root',
@@ -9,9 +8,7 @@ import { forEach } from '@angular/router/src/utils/collection';
 })
 export class AppComponent {
   title = 'shopit-app';
-  selectedImage:any[];
-  searchResultList:SearchItem[];
-  constructor(private getItemService:GetItemsService){
+  constructor(){
 //     var i:number;
 //     i=0;
 //     this.searchResultList=[];
@@ -22,33 +19,5 @@ export class AppComponent {
 // console.log("Making a sample request.....");
 //       this.getItemService.samplePost();
   }
-  handleImageUpload(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      this.searchResultList=undefined;
-        var reader = new FileReader();
-        reader.onload = (event: any) => {
-            this.selectedImage = event.target.result;
-        }
-        console.log(event.target.files[0]);
-        reader.readAsDataURL(event.target.files[0]);
-        console.log("Uploading image and querying the server.....")
-        this.getItemService.getItems(event.target.files[0]).subscribe(
-          res => {
-            console.log("The response is : ")
-            this.searchResultList=[];
-            for(var key in res){
-              console.log(res[key]);
-              var result=res[key];
-              this.searchResultList.push({name:result['name'],imgUrl:result['img'],price:result['price'],productUrl:result['href']});
-
-            }
-          },
-          err => {
-            console.log("Error occured");
-          }
-        );
-        
-
-    }
-}
+ 
 }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchItem } from '../searchresult';
+import { GetItemsService } from '../get-items.service';
 
 @Component({
   selector: 'app-main',
@@ -7,13 +8,43 @@ import { SearchItem } from '../searchresult';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  @Input()selectedImage:any[];
-  @Input()searchResultList:SearchItem[];
-  constructor() { }
+  selectedImage:any[];
+  searchResultList:SearchItem[];
+  constructor(private getItemService:GetItemsService) { }
 
 
 
   ngOnInit() {
   }
+
+  handleImageUpload(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.searchResultList=undefined;
+        var reader = new FileReader();
+        reader.onload = (event: any) => {
+            this.selectedImage = event.target.result;
+        }
+        console.log(event.target.files[0]);
+        reader.readAsDataURL(event.target.files[0]);
+        console.log("Uploading image and querying the server.....")
+        this.getItemService.getItems(event.target.files[0]).subscribe(
+          res => {
+            console.log("The response is : ")
+            this.searchResultList=[];
+            for(var key in res){
+              console.log(res[key]);
+              var result=res[key];
+              this.searchResultList.push({name:result['name'],imgUrl:result['img'],price:result['price'],productUrl:result['href']});
+
+            }
+          },
+          err => {
+            console.log("Error occured");
+          }
+        );
+    }
+}
+
+
 
 }
